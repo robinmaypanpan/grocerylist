5eedfea45330c67e491d1da600726b8dda033ef9
Robin Mayorga <robinmaypanpan@gmail.com>
2021-10-18 04:26:37 -0400

    Added sql server backend

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
-const express = require('express')
+const express = require('express');
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+// Specify our parser for incoming post commands.
+const bodyParser = require('body-parser');
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
 express()
   .use(express.static(path.join(__dirname, 'build')))
+  .use( bodyParser.json() ) // to support JSON-encoded bodies
+
   .get('/', (req, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')))
+
+  .get('/list', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM grocerylist_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.json(results);
+      res.send();
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
