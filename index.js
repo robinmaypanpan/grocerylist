c4228566aafd3901f94684f38e8940a2a6238f15
Robin Mayorga <robinmaypanpan@gmail.com>
2021-10-18 03:27:04 -0400

    Added basic heroku support

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'build')))
+  .get('/', (req, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
