50576cded399fba1b8d3c6c862607d0d7aa218cc
Robin Mayorga <robinmaypanpan@gmail.com>
2021-10-25 02:10:54 -0400

    Added database initialization to server startup

--- server/index.js
+++ server/index.js
@@ -3,13 +3,31 @@ const path = require('path');
 
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, '..', 'build')))
-  .use( express.json() ) // to support JSON-encoded bodies
+const { initializeDatabase } = require('./database');
 
-  .use('/api', require('./api'))
+async function initalize() {
+  try {
+    console.log('Initializing Database');
+    await initializeDatabase();
+  } catch (error) {
+    console.error('Failed to start server due to database error');
+    return;
+  }
 
-  // Any remainining requests are passed directly to the client so that it can do client-side routing.
-  .get('/*', (req, res) => res.sendFile(path.join(__dirname, '..', 'build', 'index.html')))
+  console.log('Database initialized');
 
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+  console.log('Starting Express server');
+
+  express()
+    .use(express.static(path.join(__dirname, '..', 'build')))
+    .use( express.json() ) // to support JSON-encoded bodies
+  
+    .use('/api', require('./api'))
+  
+    // Any remainining requests are passed directly to the client so that it can do client-side routing.
+    .get('/*', (req, res) => res.sendFile(path.join(__dirname, '..', 'build', 'index.html')))
+  
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+}
+
+initalize();
