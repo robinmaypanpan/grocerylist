9d48c4d8728fa349baddbe1b8311d629ce3de9e7
Robin Mayorga <robinmaypanpan@gmail.com>
2021-10-25 02:06:52 -0400

    Improved database to handle adding and removing, as well as initialization.

--- server/api.js
+++ server/api.js
@@ -4,15 +4,44 @@
 
 const express = require('express');
 const router = express.Router();
-const { connectAndQuery } = require('./database');
+const { getItems, addItem, removeItem } = require('./database');
 
-router.get('/list', async (req, res) => {
+router.get('/list', async (request, response) => {
     try {
-        const result = await connectAndQuery('SELECT * FROM grocerylist_table');
-        const results = result ? result.rows.map(({name}) => name) : [];
-        res.json(results);
-    } catch (err) {
-        res.send("Error " + err);
+        const items = await getItems();
+        // const results = [...Array(200)].map((val, i) => `Test Item ${i}`);
+        response.json(items);
+    } catch (error) {
+        response.send(`Error: ${error}`);
+    }
+});
+
+router.post('/addItem', async (request, response) => {
+    const { name } = request.body;
+    if (!name) {
+        console.error('Received invalid request to add item');
+        response.send('Error: No item provided');
+        return;
+    }
+
+    try {
+        const result = addItem(name);
+        response.json(result);
+    } catch (error) {
+        console.error('Failed to add item to database');
+        response.send(`Error: ${error}`);
+    }
+});
+
+router.post('/removeItem', async (request, response) => {
+    console.log(`Got removal request for ${JSON.stringify(request.body)}`);
+    const { id, name } = request.body;
+    try {
+        const result = removeItem(id, name);
+        response.json(result);
+    } catch(error) {
+        console.error('Failed to remove item from database');
+        response.send(`Error: ${error}`);
     }
 });
 
--- server/database.js
+++ server/database.js
@@ -1,5 +1,7 @@
 const { Pool } = require('pg');
 
+const GROCERY_LIST_TABLE = 'grocerylist_table';
+
 const pool = (() => {
   if (process.env.NODE_ENV !== 'production') {
       return new Pool({
@@ -31,6 +33,57 @@ function connectAndQuery(query) {
     });
 }
 
+async function initializeDatabase() {
+    return await connectAndQuery(
+        `CREATE TABLE IF NOT EXISTS ${GROCERY_LIST_TABLE} (` +
+        `id serial unique,` +
+        `name text unique,` +
+        `timestamp date,` +
+        `category_id integer ` +
+        `);`
+    );
+}
+
+async function getItems() {
+    const result = await connectAndQuery(
+        `SELECT * FROM ${GROCERY_LIST_TABLE};`
+    );
+    const results = result ? result.rows : [];
+    return results.map((item) => {
+        const newItem = {};
+        Object.keys(item).forEach((itemProp) => {
+            if (item[itemProp]) {
+                newItem[itemProp] = item[itemProp];
+            }
+        });
+        return newItem;
+    });;
+}
+
+async function addItem(name, categoryId = 0) {
+    return await connectAndQuery(
+        `INSERT INTO ${GROCERY_LIST_TABLE} (name, timestamp, category_id) \
+        VALUES ('${name}', current_timestamp, ${categoryId}) \
+        RETURNING *;`
+    ); 
+}
+
+async function removeItem(id, name) {
+    if (id) {
+        return await connectAndQuery(
+            `DELETE FROM ${GROCERY_LIST_TABLE} \
+            WHERE id = ${id} \
+            RETURNING *;`
+        );
+    } else if (name) {
+    } else {
+        throw 'No removal key provided';
+    }
+}
+
 module.exports = {
-    connectAndQuery
+    initializeDatabase,
+    getItems, 
+    addItem,
+    removeItem
 };
