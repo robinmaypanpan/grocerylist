51e568a1a69ec9d79e3c420dca356dddddc550aa
Robin Mayorga <robinmaypanpan@gmail.com>
2021-10-27 02:33:00 -0400

    Significantly updated server code

--- server/api.js
+++ server/api.js
@@ -4,45 +4,114 @@
 
 const express = require('express');
 const router = express.Router();
-const { getItems, addItem, removeItem } = require('./database');
 
-router.get('/list', async (request, response) => {
+const { 
+    createList,
+    updateList,
+    removeList,
+    getList, 
+
+    getCategories,
+    removeCategory,
+    updateCategory,
+
+    addItem,
+    updateItem,
+    removeItem 
+} = require('./database');
+
+router.get('/getList', async (request, response) => {
+    const { listId } = request.query;
     try {
-        const items = await getItems();
-        // const results = [...Array(200)].map((val, i) => `Test Item ${i}`);
-        response.json(items);
+        const result = await getList(listId);
+        response.json(result);
     } catch (error) {
-        response.send(`Error: ${error}`);
+        response.json({success: false, error});
     }
-});
+}).post('/createList', async (request, response) => {
+    const { name } = request.body;
+    try {
+        const listId = await createList(name);
+        response.json({listId});
+    } catch (error) {
+        response.json({success: false, error});
+    }
+}).put('/updateList', async (request, response) => {
+    const { name } = request.body;
+    try {
+        const result = await updateList(name);
+        response.json(result);
+    } catch (error) {
+        response.json({success: false, error});
+    }
+}).delete('removeList', async (request, response) => {
+    const { listId } = request.body;
+    try {
+        await removeList(listId);
+        response.json({success: true});
+    } catch (error) {
+        response.json({success: false, error});
+    }
+})
 
 router.post('/addItem', async (request, response) => {
-    const { name } = request.body;
-    if (!name) {
+    const { name, listId, categoryName } = request.body;
+    if (!name || !listId) {
         console.error('Received invalid request to add item');
-        response.send('Error: No item provided');
+        response.json({error: 'Insufficient data to create item'});
         return;
     }
 
     try {
-        const result = await addItem(name);
+        const result = await addItem(name, listId, categoryName);
         response.json(result);
     } catch (error) {
         console.error('Failed to add item to database');
-        response.send(`Error: ${error}`);
+        response.json({success: false, error});
     }
-});
-
-router.post('/removeItem', async (request, response) => {
-    console.log(`Got removal request for ${JSON.stringify(request.body)}`);
-    const { id, name } = request.body;
+}).put('/updateItem', async (request, response) => {
+    const { itemId, name, listId, categoryName } = request.body;
     try {
-        const result = await removeItem(id, name);
+        const result = await updateItem(itemId, name, listId, categoryName);
+        response.json(result);
+    } catch (error) {
+        response.json({error});
+    }
+}).delete('/removeItem', async (request, response) => {
+    const { itemId, listId } = request.body;
+    try {
+        const result = await removeItem(itemId, listId);
         response.json(result);
     } catch(error) {
         console.error('Failed to remove item from database');
-        response.send(`Error: ${error}`);
+        response.json({success: false, error});
     }
 });
+
+router.get('/getCategories', async (request, response) => {
+    const { listId } = request.query;
+    try {
+        const result = await getCategories(listId);
+        response.json(result);
+    } catch (error) {
+        response.json({success: false, error});
+    }
+}).put('/updateCategory', async (request, response) => {
+    const { name, categoryId, listId } = request.body;
+    try {
+        const result = await updateCategory(name, categoryId, listId);
+        response.json(result);
+    } catch (error) {
+        response.json({success: false, error});
+    }
+}).delete('removeCategory', async (request, response) => {
+    const { categoryId, listId } = request.body;
+    try {
+        const result = await removeCategory(categoryId, listId);
+        response.json(result);
+    } catch (error) {
+        response.json({success: false, error});
+    }
+})
 
 module.exports = router;
