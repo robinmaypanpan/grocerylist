e80fb5e7d7d16c1fdb2c74d0843838c787e2f71c
orikalin <orikalin@gmail.com>
2021-11-02 16:21:47 -0400

    flexbox to align checkboxes

--- src/components/ItemDisplay.jsx
+++ src/components/ItemDisplay.jsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
 import styled from "styled-components"
 const ItemDiv = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+    justify-content: space-between;
+    flex-basis: auto;
     width: 100%;
     color: ${props=>props.theme.dataText};
     border-style: solid;
@@ -12,21 +17,23 @@ const ItemSpan = styled.span`
     display: inline-block;
     vertical-align: middle;
     line-height: normal;
-    margin: 0.7em 0.1em;
+    margin: 0.7em 0.5em;
     text-decoration: ${props=>props.strikeText?'line-through':''};
 `;
 const CheckBox  = styled.input`
-text-align: right;
-transform: scale(2);
+    transform: scale(2);
+    margin-right: 1.5em;
 `
 function ItemDisplay({item})
 {
     const [isChecked, setChecked] = useState(false);
+
     function handleOnChange(){
         setChecked(!isChecked);
     }
+
     return (
-        <ItemDiv>
+        <ItemDiv onClick={handleOnChange}>
             <ItemSpan strikeText={isChecked}>
                 {item.name}
             </ItemSpan>
