8fcb8f6579c4070c5375da67b28611949487499e
Robin Mayorga <robinmaypanpan@gmail.com>
2021-11-02 20:24:40 -0400

    Improved sorting to prevent items jumping around.

--- server/database/list.js
+++ server/database/list.js
@@ -57,7 +57,7 @@ async function removeList(listId) {
 async function getList(listId) {
     const [items, name] = await connectAndQuery([
         {
-            query: `SELECT id, name, timestamp, category_id, checked FROM ${ITEM_TABLE} WHERE list_id=$1 ORDER BY timestamp DESC;`,
+            query: `SELECT id, name, timestamp, category_id, checked FROM ${ITEM_TABLE} WHERE list_id=$1 ORDER BY timestamp DESC, category_id, id;`,
             values: [listId]
         },
         {
