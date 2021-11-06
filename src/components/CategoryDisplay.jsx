0f335ef88789b1515aaac9d0ce9823362a341839
Robin Mayorga <robinmaypanpan@gmail.com>
2021-11-06 00:53:30 -0400

    Visual adjustments

--- src/components/CategoryDisplay.jsx
+++ src/components/CategoryDisplay.jsx
@@ -1,17 +1,11 @@
 import styled from "styled-components";
 
 const Container = styled.div`
-    margin:5px;
-    color: ${props => props.theme.dataText};
-    border: ${props => props.theme.dataBorder};
-    border-radius: ${props => props.theme.dataBorderRadius};  
+    margin:5px; 
 `;
 
-const Label = styled.span`
-    position: relative;
-    top: -9px;
-    left: 4px;
-    padding: 2px 5px;
+const Label = styled.div`
+    padding: 4px 5px;
 
     font-size: 1.2em;
     font-weight: bold;
