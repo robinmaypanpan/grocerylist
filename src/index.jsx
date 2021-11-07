f291b30977de96a7f9e0aadf4fb0ed0944e5c439
Robin Mayorga <robinmaypanpan@gmail.com>
2021-11-07 01:53:55 -0400

    Removed unnecessary debug date thing

--- src/index.jsx
+++ src/index.jsx
@@ -29,7 +29,7 @@ const GlobalStyle = createGlobalStyle`
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ThemeProvider theme={themeSelector(new Date())}>
+      <ThemeProvider theme={themeSelector()}>
         <GlobalStyle/>
         <Router/>
       </ThemeProvider>
